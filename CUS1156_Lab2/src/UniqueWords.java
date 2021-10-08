4b6eb6f3e64dd575ecab27168e4b5c3ee7785eef
scannaluc <92131814+scannaluc@users.noreply.github.com>
2021-10-08 15:54:00 -0400

    Add files via upload

---
+++ CUS1156_Lab2/src/UniqueWords.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class UniqueWords
+{
+   /**
+		counts the number of unique strings in a list
+		@param list ArrayList of strings to be examined
+		@return number of unique strings in the list
+   */
+	   public static int countUnique(ArrayList<String> list)
+	   {
+		  int count = 0;
+		  
+	      for (int i = 0; i < list.size(); i++)
+	      {		
+	    	  int countUnique = 0;
+	    	  for (int j = 0; j < i; j++)
+			 
+	      	{
+				if(list.get(j)==(list.get(i))){
+					countUnique++;
+				}
+			
+			 }
+	      if(countUnique==0) {
+	    	  count++;
+	      }
+	      
+	      }
+		  return count;
+	   }
+
+   public static void main(String[] args)
+   {
+      ArrayList <String> words = new ArrayList<>();
+      words.add("apple");
+	  words.add("orange");
+	  words.add("blackboard");
+	  words.add("apple");
+	  words.add("orange");
+	  words.add("sun");
+	  words.add("moon");
+   
+	  int unique = countUnique(words);
+      System.out.println(words + " has " + unique + " unique words");
+   }
+}
